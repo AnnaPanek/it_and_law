7e171f3be4cd0557a7a26983464849fb58fb5d40
Anna Panek <anna.panek@gmail.com>
2013-09-01 11:56:19 +1000

    First commit with updated Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItAndLaw::Application.config.secret_key_base = 'dbcfbe5454fcb314d139bcfff55e263feabb13e7b48784efc56f7512ac4a9ad79e3924aa941ce676ecc83d7be1cbef969e34faf2aa84a82d0d95191d33f8f750'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ItAndLaw::Application.config.session_store :cookie_store, key: '_it_and_law_session'
